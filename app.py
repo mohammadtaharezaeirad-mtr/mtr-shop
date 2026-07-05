8da9ed31cebf6241a75ebdc30331f1a40fd5850f
mohammad_taha <mohammadtaha.rezaeirad@gmail.com>
2026-07-05 15:27:54 +0330

    add route admin_login

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/admin/login')
+def admin_login():
+    return render_template('admin_login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
     
