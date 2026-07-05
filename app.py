9c94855a57eb2a7082ffd9029011241629f32125
mohammad_taha <mohammadtaha.rezaeirad@gmail.com>
2026-07-06 01:37:08 +0330

    add database products in file app.py

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 from flask import Flask,render_template,request,session,redirect,abort
 import config
+import pymysql
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = config.SECRET_KEY
+
+
+def database():
+    conn = pymysql.connect(
+        host="127.0.0.1",
+        user="root",
+        password="root",  
+        )
+    cursor = conn.cursor()
+    
+    
+    cursor.execute("CREATE SCHEMA IF NOT EXISTS `mtr_shop`")
+    cursor.execute("CREATE TABLE `mtr shop`.`products` (\
+  `id` INT NOT NULL AUTO_INCREMENT,\
+  `name` VARCHAR(200) NOT NULL,\
+  `prce` INT NULL,\
+  `description` TEXT NULL,\
+  `active` INT NULL,\
+  `productscol` VARCHAR(45) NULL,\
+  PRIMARY KEY (`id`),\
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);")
+    conn.commit()
+    
+database()
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -28,4 +54,5 @@ def dashboard():
 
 if __name__ == '__main__':
     app.run(debug = True)
+
     
