cd806b6db83989e76d9ac26f9d8f75f6f9c2bfdf
mohammad_taha <mohammadtaha.rezaeirad@gmail.com>
2026-07-08 21:26:47 +0330

    Improving page names

--- config.py
+++ config.py
@@ -4,12 +4,19 @@ PASSWORD_ADMIN = '12345'
 
 SECRET_KEY = 'fskdfjalkjf;lajfoefjal;jdflasjf;joeifsjj;weojf;jweofj;oj;oajjlkjf;osajfo;ejf;kdjhjvjhfvfvb'
 
-
+#_____start name pages home_____#
 name_page_home = 'home'
+name_page_view_products = 'view_products'
+#_____ name pages home_____#
+
+#_____start name pages admin_____#
 name_page_products = 'products'
 name_page_dashboard = 'dashboard'
 name_page_admin_login = 'admin_login'
 name_page_editproducts = 'edit_products'
-name_page_view_products = 'view_products'
-name_page_sign_up = 'sign_up.html'
-name_page_login = 'login.html'
+#_____end name pages admin_____#
+
+#_____start name pages users_____#
+name_page_sign_up = 'sign_up'
+name_page_login = 'login'
+#_____end name pages users_____#
