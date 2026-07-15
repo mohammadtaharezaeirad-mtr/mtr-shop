ee3c700b980862b86f93a0af88b1c6a733355337
mohammad_taha <mohammadtaha.rezaeirad@gmail.com>
2026-07-15 12:58:01 +0330

    add slider

---
+++ static/js/home.js
@@ -0,0 +1,17 @@
+const slider = document.querySelector('#main_slider')
+
+let trans = 0
+setInterval(() => {
+    if(trans == 0){
+        trans = 100
+        slider.style.transform = 'translateX(100%)';
+    }else if(trans == 100){
+        trans = 200
+        slider.style.transform = 'translateX(200%)';
+    }else if(trans == 200){
+        trans = 0
+        slider.style.transform = 'translateX(0%)';
+    }
+
+
+},5000)
