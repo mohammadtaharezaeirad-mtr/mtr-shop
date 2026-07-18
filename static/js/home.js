47969efb85412172bd81afe7ecd6cfebdc345c7f
mohammad_taha <mohammadtaha.rezaeirad@gmail.com>
2026-07-18 17:55:34 +0330

    add view products

--- static/js/home.js
+++ static/js/home.js
@@ -17,3 +17,24 @@ setInterval(() => {
 
 },4000)
 //end slider
+
+//start main products
+const main_products = document.querySelector('#main_products')
+const btn_right_view = document.querySelector('#btn_right_view')
+const btn_left_view = document.querySelector('#btn_left_view')
+
+let trans_btn_right_left = 10
+btn_right_view.addEventListener('click', () => {
+    if(trans_btn_right_left != 100){
+    trans_btn_right_left += 10
+    main_products.style.transform = `translateX(${trans_btn_right_left}%)`
+    }
+})
+
+btn_left_view.addEventListener('click' , () => {
+    if(trans_btn_right_left  != -100){
+    trans_btn_right_left -= 10
+    main_products.style.transform = `translateX(${trans_btn_right_left}%)`
+    }
+})
+//end main products
