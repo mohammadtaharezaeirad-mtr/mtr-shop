11b4f06b436da2a6d0f1210ce26a6793b9e61ecd
mohammad_taha <mohammadtaha.rezaeirad@gmail.com>
2026-07-21 15:08:49 +0330

    add update prce  in prce_products

--- static/js/view_products.js
+++ static/js/view_products.js
@@ -2,14 +2,21 @@
 let quantity_add = document.querySelector('.quantity_add')
 let quantity_remove = document.querySelector('.quantity_remove')
 let main_quantity =  document.querySelector('.main_quantity')
+let main_prce_products = document.querySelector('.main_prce_products')
 
+
+main_prce_products_number = Number(main_prce_products.innerText)
+main_prce_products1 = Number(main_prce_products.innerText)
 quantity_add.addEventListener('click' , () => {
     let value_quantity_add = main_quantity.value
     value_quantity_add = Number(value_quantity_add)
     value_quantity_add += 1
-    
+
     if(main_quantity.value != main_quantity.max){
         main_quantity.value = value_quantity_add
+        main_prce_products_number += main_prce_products1
+        main_prce_products.textContent = main_prce_products_number
+
     }
 })
 
@@ -19,6 +26,8 @@ quantity_remove.addEventListener('click' , () => {
     value_quantity_remove -= 1
     if(value_quantity_remove != quantity_remove.min){
         main_quantity.value = value_quantity_remove
+        main_prce_products_number -= main_prce_products1
+        main_prce_products.textContent = main_prce_products_number
     }
 })
 // end add and remove quantity
